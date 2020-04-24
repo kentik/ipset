f4d21bee55c295688c3a7d8b8a9e8be005308c8e
Tomasz Jonak <tcjonak@gmail.com>
2020-04-24 20:48:45 +0200

    IPSet: add stub IPv6 tree

--- treeV6.go
+++ treeV6.go
@@ -6,13 +6,20 @@ type ipv6Node struct {
 }
 
 type treeV6 struct {
-	nodes []ipv6Node
+	// nodes []ipv6Node
+	nets []*net.IPNet
 }
 
 func (t *treeV6) Add(cidr *net.IPNet) {
+	t.nets = append(t.nets, cidr)
 	return
 }
 
 func (t *treeV6) Contains(ip net.IP) bool {
+	for _, net := range t.nets {
+		if net.Contains(ip) {
+			return true
+		}
+	}
 	return false
 }
